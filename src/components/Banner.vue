95f307cba21359785056434d33f9a283c98b632b
bluethundergames <whiterabbitwebstudios@gmail.com>
2024-12-12 21:22:13 -0500

    Fonts and Base Styles Added, Header/Footer/Banner Development

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
-
+import Button from './Button.vue'
 </script>
 <template>
-    
+    <section class="banner border-b border-white">
+        <div class="container mx-auto">
+            <div class="lg:flex lg:justify-between">
+                <div class="lg:w-6/12 py-20 pr-4 lg:border-r border-white">
+                    <h1>RareBits</h1>
+                    <div class="img-container rounded-lg">
+                        <img src="/src/assets/img/banner-bird.png" alt="Bird" width="394" height="591" />
+                    </div>
+                </div>
+                <div class="lg:w-6/12 py-20 pl-16">
+                    <div class="banner-heading pb-20">
+                        <h2>Unleash the <span class="pink">power</span> of digital creativity: Explore the world of <span class="pink">unique</span> NFTS.</h2>
+                    </div>     
+                    <div class="banner-copy mb-14 pt-10">
+                        <p>Discover, Collect, and Trade rare digital assets that showcase the boundless potential of the blockchain.</p>
+                    </div>
+                    <Button text="View Collections" link="/some-url"></Button>
+                </div>  
+            </div>
+        </div>
+    </section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
   content: [
     './index.html',
@@ -6,20 +8,36 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: 
+    {
+      'azo': ['azo-sans-web', 'sans-serif'],
+      'monda': ['Monda', 'sans-serif'],
+      'mont': ['Montserrat', 'sans-serif'],
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "4rem",
+      },
+      width: {
+        DEFAULT: "100%",
+        xl: "1440px",
+      }
+    },
+    colors: {
+      'theme-black' : '#050104',
+      'theme-pink' : '#FD06B2',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    }
   },
   plugins: [
-    function({ addUtilities }) {
-      addUtilities({
-        '.mx-auto': {
-          'margin-left': 'auto',
-          'margin-right': 'auto',
-          'padding-left': '64px',
-          'padding-right': '64px',
-          'width': '1520px',
-          'max-width': '100%',
-        },
-      }, ['responsive', 'hover']);
-    },
   ],
 }
 
